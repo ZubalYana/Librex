b07f42bae9b5ad7cca758baa2d868f707ae61372
Yana <zubalana0@gmail.com>
2026-07-17 10:28:07 +0300

    feat(backend): handle pagination on backend, put the req.query together and send proper json to the client

--- apps/backend/src/controllers/books.ts
+++ apps/backend/src/controllers/books.ts
@@ -1,17 +1,59 @@
 import type { Request, Response } from "express";
 import { getPrisma } from "../utils/db.js";
 
-export async function getAllBooks(res: Response){
+export async function getAllBooks(req: Request, res: Response){
     try{
         const prisma = getPrisma();
-        const books = await prisma.book.findMany();
-        if(!books){
-            res.status(404).json({message: 'Books not found'});
-            return;
+        const search = req.query.search as string | undefined;
+        const order = req.query.order === 'desc' ? 'desc' : 'asc';
+         const where = search
+      ? {
+          OR: [
+            { name: { contains: search, mode: 'insensitive' as const } },
+            { author: { contains: search, mode: 'insensitive' as const } },
+          ],
         }
-        res.status(200).json({books});
+      : {};
+        const page = Number(req.query.page) || 1;
+        const booksLimit = Number(req.query.booksLimit) || 10;
+
+        const skip = ( page - 1 ) * booksLimit;
+    
+        const [books, totalCount] = await Promise.all([ 
+            prisma.book.findMany({where, orderBy: {name: order}, skip, take: booksLimit}),
+            prisma.book.count({where})
+            ])
+
+        res.status(200).json({
+            books,
+            pagination: {
+                page,
+                booksLimit, 
+                totalCount,
+                totalPages: Math.ceil(totalCount/booksLimit),
+            },
+        });
     }catch(err){
         const message = err instanceof Error? err.message : 'Unknown error while fetching books';
+        res.status(500).json({message: message});
+    }
+}
+
+export async function getBookDetails(req: Request, res: Response){
+    try{
+        const prisma = getPrisma();
+        const bookId = req.params.bookId;
+        
+        const book = await prisma.book.findUnique({where: {id: String(bookId)}});
+
+        if(!book){
+            res.status(404).json({message: 'Book not found'});
+            return;
+        }
+
+        res.status(200).json(book);
+    }catch(err){
+        const message = err instanceof Error? err.message : 'Unknown error while getting book details';
         res.status(500).json({message: message});
     }
 }
