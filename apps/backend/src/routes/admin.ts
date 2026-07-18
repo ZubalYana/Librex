0387487dacc9b04c67bd756eee56887192a5227f
Yana <zubalana0@gmail.com>
2026-07-18 17:16:20 +0300

    fix(backend): fix admin route namings

--- apps/backend/src/routes/admin.ts
+++ apps/backend/src/routes/admin.ts
@@ -4,9 +4,9 @@ import AuthMiddleware from "../middleware/authMiddleware.js";
 const router = Router();
 
 router.get('/users', AuthMiddleware, getAllUsers);
-router.delete('/:userId', AuthMiddleware, deleteUser);
-router.delete('/:bookId', AuthMiddleware, deleteUsersBook);
-router.delete('/:userId', AuthMiddleware, deleteAllUsersBooks);
-router.put('/:userId', AuthMiddleware, editUserRole);
+router.delete('/users/:userId', AuthMiddleware, deleteUser);
+router.delete('/books/:bookId', AuthMiddleware, deleteUsersBook);
+router.delete('/users/:userId/books', AuthMiddleware, deleteAllUsersBooks);
+router.put('/users/:userId/role', AuthMiddleware, editUserRole);
 
 export default router;
