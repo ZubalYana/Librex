0f8e9b3f696948c0b9fa93bc408cb04ea3f1842d
Yana <zubalana0@gmail.com>
2026-07-18 07:13:45 +0300

    feat(backend): create controllers to delete a user and delete all books of a user ( admin only functionality )

--- apps/backend/src/controllers/admin.ts
+++ apps/backend/src/controllers/admin.ts
@@ -43,6 +43,32 @@ export async function getAllUsers(req: Request, res: Response) {
   }
 }
 
+export async function deleteUser(req: Request, res: Response){
+    try{
+        const prisma = getPrisma();
+
+        if(req.user?.role !== 'ADMIN'){
+            res.status(403).json({message: 'Forbidden access'});
+            return;
+        }
+
+        const userId = req.params.userId;
+
+        const user = await prisma.user.findUnique({where: { id: String(userId)}});
+
+        if(!user){
+            res.status(404).json({message: 'User to be deleted not found'});
+            return;
+        }
+
+        await prisma.user.delete({where: { id: String(userId)}});
+        res.status(200).json({message: 'User deleted successfully'});
+    }catch(err){
+        const message = err instanceof Error? err.message : 'Unknown error while trying to delete user';
+        res.status(500).json({message: message});
+    }
+}
+
 export async function deleteUsersBook(req: Request, res: Response){
     try{
         const prisma = getPrisma();
@@ -64,6 +90,33 @@ export async function deleteUsersBook(req: Request, res: Response){
         res.status(200).json({message: 'Book deleted successfully'});
     }catch(err){
         const message = err instanceof Error? err.message : 'Unknown error while deleting user\'s book';
+        res.status(500).json({message: message});
+    }
+}
+
+export async function deleteAllUsersBooks(req: Request, res: Response){
+    try{
+        const prisma = getPrisma();
+
+        if(req.user?.role !== 'ADMIN'){
+            res.status(403).json({message: 'Forbidden access'});
+            return;
+        }
+
+        const userId = req.params.userId;
+
+        const usersBooks = await prisma.book.findMany({where: {ownerId: String(userId)}});
+
+        if(usersBooks.length === 0){
+            res.status(404).json({message: 'User\'s books not found'});
+            return;
+        }
+
+        await prisma.book.deleteMany({where: {ownerId: String(userId)}});
+
+        res.status(200).json({message: 'User\'s books have been deleted successfully'});
+    }catch(err){
+        const message = err instanceof Error? err.message : 'Unknown error while trying to delete user\'s books';
         res.status(500).json({message: message});
     }
 }
