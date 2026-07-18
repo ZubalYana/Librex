7176351e05a3959bbf4c0036b3c6c4c1e42bb76f
Yana <zubalana0@gmail.com>
2026-07-18 07:04:43 +0300

    feat(backend): create a controller for admin to delete users book

--- apps/backend/src/controllers/admin.ts
+++ apps/backend/src/controllers/admin.ts
@@ -42,3 +42,28 @@ export async function getAllUsers(req: Request, res: Response) {
     res.status(500).json({ message: message });
   }
 }
+
+export async function deleteUsersBook(req: Request, res: Response){
+    try{
+        const prisma = getPrisma();
+        if(req.user?.role !== 'ADMIN'){
+            res.status(403).json({message: 'Forbidden access'});
+            return;
+        }
+
+        const bookId = req.params.bookId;
+
+        const book = await prisma.book.findUnique({where: {id: String(bookId)}});
+        if(!book){
+            res.status(404).json({message: 'Book to be deleted not found'});
+            return;
+        }
+
+        await prisma.book.delete({where: {id: String(bookId)}});
+
+        res.status(200).json({message: 'Book deleted successfully'});
+    }catch(err){
+        const message = err instanceof Error? err.message : 'Unknown error while deleting user\'s book';
+        res.status(500).json({message: message});
+    }
+}
