d0ab7f53eccdd4730704722f4f6c4bde79d6930a
Yana <zubalana0@gmail.com>
2026-07-18 04:59:00 +0300

    feat(backend): create a controller for user to get his private profile info, omit password

---
+++ apps/backend/src/controllers/user.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import { getPrisma } from "../utils/db.js";
+
+export async function userPrivateProfile(req: Request, res: Response){
+    try{
+        const prisma = getPrisma();
+        const userId = req.user?.userId;
+
+        const user = await prisma.user.findUnique({where: {id: String(userId)}});
+
+        if(!user){
+            res.status(404).json({message: 'User not found'});
+            return;
+        }
+
+        const {password: _, ...userWithoutPassword} = user;
+        res.status(200).json({userWithoutPassword});
+    }catch(err){
+        const message = err instanceof Error? err.message : 'Unknown error while fetching user';
+        res.status(500).json({message: message});
+    }
+}
