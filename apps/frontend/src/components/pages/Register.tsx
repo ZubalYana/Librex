d8b662a77e92a63c38cbd47d3becfca1829115e1
Yana <zubalana0@gmail.com>
2026-07-20 05:45:59 +0300

    feat(frontend): create a component for nice logo display, modify the registration and login pages: add an illustration and change flexbox layout

--- apps/frontend/src/components/pages/Register.tsx
+++ apps/frontend/src/components/pages/Register.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/Button";
 import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../../api/apiFetch";
 import { useAuthStore } from "../../store/authStore";
+import CombinedLogo from "../ui/CombinedLogo";
 
 export default function Register(){
     const [name, setName] = useState<string>('');
@@ -37,7 +38,11 @@ export default function Register(){
     }
 
     return(
-        <div className="text-[#2B2D42] w-full h-screen p-[20px] flex jusitfy-center items-center md:p-[40px] md:justify-between">
+        <div className="text-[#2B2D42] w-full h-screen p-[20px] flex flex-col md:p-[40px] n">
+            <div className="w-full">
+                <CombinedLogo/>
+            </div>
+            <div className="w-full flex flex-1 items-center md:justify-between">
             <div className="w-full md:w-[400px] min-h-[300px] bg-[#FDFBF7] shadow-md p-[20px] md:p-[30px] rounded-md">
                 <h3 className="text-[24px] font-bold mb-4">Register</h3>
                 <div className="flex flex-col gap-y-3">
@@ -47,6 +52,10 @@ export default function Register(){
                 </div>
                 <Button onClick={()=>onRegister()} isLoading={loading} className="mt-4 w-full" variant="primary" size="md" disabled={disabled}><p className="uppercase text-[18px] font-semibold text-parchment">Register</p></Button>
                 <p className="w-full flex justify-center cursor-pointer text-[12px] text-accent mt-2" onClick={()=>navigate('/login')}>Already a user? <span className="underline ml-1"> Log in.</span></p>
+            </div>
+            <div className="hidden md:flex w-[50%] flex items-center justify-center">
+                <img src="/illustrations/Booklover-bro.svg" alt="illustration" className="w-[500px]" />
+            </div>
             </div>
         </div>
     )
---
+++ apps/frontend/src/components/ui/CombinedLogo.tsx
@@ -0,0 +1,8 @@
+export default function CombinedLogo(){
+    return(
+        <div className="flex gap-x-2 items-center">
+            <img src="/LibrexLogo.svg" alt="librexLogo" className="max-w-[30px]" />
+            <h4 className="text-[18px] font-semibold">Librex</h4>
+        </div>
+    )
+}
