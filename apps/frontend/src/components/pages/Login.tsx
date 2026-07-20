19e6d307f0c0eb4b7ff5cbca71d95d9b20696c2c
Yana <zubalana0@gmail.com>
2026-07-20 05:34:01 +0300

    feat(frontend): create a request to log user in

--- apps/frontend/src/components/pages/Login.tsx
+++ apps/frontend/src/components/pages/Login.tsx
@@ -3,15 +3,31 @@ import { Input } from "../ui/Input"
 import { PasswordInput } from "../ui/PasswordInput";
 import { Button } from "../ui/Button";
 import { useNavigate } from "react-router-dom";
+import { apiFetch } from "../../api/apiFetch";
+import { useAuthStore } from "../../store/authStore";
 
 export default function Login(){
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(false);
 
-
     const disabled = !email || !password;
     const navigate = useNavigate();
+
+    const onLogin = ()=>{
+        setLoading(true);
+        const data = JSON.stringify({email: email, password: password})
+        apiFetch('/auth/login', {
+            method: 'POST',
+            body: data
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
+            useAuthStore.getState().setAuth(data.user, data.token);
+            setLoading(false);
+            navigate('/app/books');
+        }).catch((err)=>console.error(err));
+    }
 
     return(
         <div className="text-[#2B2D42] w-full h-screen p-[20px] flex jusitfy-center items-center md:p-[40px] md:justify-between">
@@ -21,7 +37,7 @@ export default function Login(){
                 <Input placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} />
                 <PasswordInput placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} showStrength={false}/>
                 </div>
-                <Button className="mt-4 w-full" variant="primary" size="md" disabled={disabled}><p className="uppercase text-[18px] font-semibold text-parchment">Login</p></Button>
+                <Button onClick={()=>onLogin()} isLoading={loading} className="mt-4 w-full" variant="primary" size="md" disabled={disabled}><p className="uppercase text-[18px] font-semibold text-parchment">Login</p></Button>
                 <p className="w-full flex justify-center cursor-pointer text-[12px] text-accent mt-2" onClick={()=>navigate('/register')}>Not yet a user? <span className="underline ml-1">Register.</span></p>
             </div>
         </div>
